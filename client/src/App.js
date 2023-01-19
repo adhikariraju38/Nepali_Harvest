31ffdaad1310bbdf6dd25e2fdd895f94bfaa0771
Rajeev Yadav <rajeevy396@gmail.com>
2023-01-19 19:23:54 +0545

    slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+import SliderComponent from './Components/Slider/Slider';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <SliderComponent/>
     </div>
   );
 }
