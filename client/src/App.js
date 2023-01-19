7a13dd27f496b2bf3f4b1e024f442944ed1ab92d
Rajeev Yadav <rajeevy396@gmail.com>
2023-01-19 23:15:58 +0545

    PlantDoctor added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SliderComponent from './Components/Slider/Slider';
 import SeasonalCrop from './Components/SeasonalCropRec/SeasonalCrop';
 import Footer from './Components/Footer/Footer';
+import PlantDoctor from './Components/PlantDoctor/PlantDoctor';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <SliderComponent/>
       <SeasonalCrop/>
+      <PlantDoctor/>
       <Footer/>
     </div>
   );
